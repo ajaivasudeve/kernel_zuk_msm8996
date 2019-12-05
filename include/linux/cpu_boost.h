24b49dbe7061db4c8fd88dc56be32319811942bc
ajaivasudeve <ajaivasudeve@gmail.com>
2019-12-05 14:38:49 +0530

    cpu-boost: reformat code
    Signed-off-by: ajaivasudeve <ajaivasudeve@gmail.com>

--- include/linux/cpu_boost.h
+++ include/linux/cpu_boost.h
@@ -2,21 +2,26 @@
 #define _CPU_BOOST_H_
 
 #ifdef CONFIG_CPU_BOOST
-void do_dsb_kick(void);
+void do_dsb_kick (void);
 extern unsigned long last_input_time;
-void input_boost_max_kick(unsigned int duration_ms);
-void mdss_boost_kick(void);
+void input_boost_max_kick (unsigned int duration_ms);
+void mdss_boost_kick (void);
 extern unsigned int smart_load;
 extern unsigned int sb_damp_factor;
 #else
-static inline void do_dsb_kick(void)
+static inline void
+do_dsb_kick (void)
 {
 }
-static inline void input_boost_max_kick(unsigned int duration_ms)
+
+static inline void
+input_boost_max_kick (unsigned int duration_ms)
 {
 }
-static inline void mdss_boost_kick(void)
+
+static inline void
+mdss_boost_kick (void)
 {
 }
 #endif
-#endif 
+#endif
